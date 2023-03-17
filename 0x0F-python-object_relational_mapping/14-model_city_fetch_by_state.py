3b094d8ba50af5ee3532ca6debb85d5cb37dd51c
Bekalu Geta <bekalu2054@gmail.com>
2023-03-17 21:09:49 +0300

    ofcourse

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-'''Prints all city objects and their state in a database.
+'''Prints all City objects and their State in a database.
 '''
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import relationship, sessionmaker
 
 from model_state import Base, State
 from model_city import City
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    passwd = sys.argv[2]
+    pword = sys.argv[2]
     db = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         user,
-        passwd,
+        pword,
         db)
         )
-    State.cities = relationship('City', back_populates='state')
+    State.cities = relationship('City', back_populate='state')
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
     result = session.query(City).order_by(City.id.asc()).all()
     for row in result:
-        print('{}: ({}) {}'.format(row.state.name, row.id, row.name))
+        print('{}: ({}) {}'.format(row.state.name, row.id, row.name)
