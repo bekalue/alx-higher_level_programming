eceae989774f38cb9618c2aa0b2ba7c194aab89b
Bekalu Geta <bekalu2054@gmail.com>
2023-03-17 18:13:54 +0300

    nice

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-'''prints the id of a state object with a given state name in a database table.
+'''Prints the id of a state object with a given name in a database table.
 '''
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from model_state import State, Base
+from model_state import Base, State
 
 
-if __name__ == "__main__":
-    user = sys.argv[1]
-    pword = sys.argv[2]
-    db_name = sys.argv[3]
+if __name__ == '__main__':
     state_name = sys.argv[4]
-    chk = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
-    if not all(chk):
+    check = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
+    if not all(check):
         state_name = ''
-    DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
-        user, pword, db_name
-        )
+    DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(DATABASE_URL)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
     result = session.query(State).filter(State.name == state_name).first()
-    if result is not None:
-        print('{}'.format(result.id))
-    else:
+    if result is None:
         print('Not found')
+    else:
+        print(f'{result.id}')
