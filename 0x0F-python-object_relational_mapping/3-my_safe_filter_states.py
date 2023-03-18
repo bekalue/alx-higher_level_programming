c74f62e5f6699a3d195c647c183f09b239b33dfa
Bekalu Geta <bekalu2054@gmail.com>
2023-03-18 16:59:38 +0300

    lol fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
+'''Prints all rows in the states table of a database \
+with a name that matches the given argument and \
+is safe from SQL injection.
+'''
 import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        db_connection = MySQLdb.connect(
             host='localhost',
             port=3306,
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3]
-            )
-    cur = db.cursor()
-    state_name = sys.argv[4]
-    cur.execute(
-            "SELECT * FROM states WHERE name=%s" +
-            "ORDER BY states.id ASC;", [state_name]
-            )
-    [print(result) for result in cur.fetchall()]
-    cur.close()
-    db.close()
+        )
+        cursor = db_connection.cursor()
+        state_name = sys.argv[4]
+        cursor.execute(
+            'SELECT * FROM states WHERE CAST(name AS BINARY) ' +
+            'LIKE %s ORDER BY id ASC;',
+            [state_name]
+        )
+        results = cursor.fetchall()
+        for result in results:
+            print(result)
+        db_connection.close()
