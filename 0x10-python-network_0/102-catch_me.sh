5a4fa6c4e0c3041adb7b53c2f0b1061e8f68a101
Bekalu Endrias <bekalue2054@gmail.com>
2023-04-01 02:27:59 +0000

    🔧Fix root errors and comment

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Follows a URL for it to respond with "You got me!"
-curl -sx PUT -H "Origin: HolbertonSchool" -d "user_id=98" --max-redris -1 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" --max-redris -1 0.0.0.0:5000/catch_me
