60ca7236c238a1bbe675e3ea0c40cfad449965e2
Bekalu Geta <bekalu2054@gmail.com>
2023-04-01 14:57:17 +0300

    Do first task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches a URL"""
+import urllib.request as req
+
+if __name__ == "__main__":
+    with req.urlopen('https://https://alx-intranet.hbtn.io/status') as r:
+        if r.status == 200:
+            data = r.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(data)))
+            print('\t- content: {}'.format(data))
+            print('\t- utf8 content: {}'.format(data.decode()))
+
