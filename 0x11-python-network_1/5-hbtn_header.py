ab93d5a58f3d7b01a5b779c96eab2e3674d48f8c
Bekalu Endrias <bekalue2054@gmail.com>
2023-04-01 13:36:11 +0000

    📝Write a script fetch header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches a header of a response from URL."""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        response = requests.get(sys.argv[1])
+        if 'X-Request-Id' in requests.headers:
+            print(requests.headers['X-Request-Id'])
