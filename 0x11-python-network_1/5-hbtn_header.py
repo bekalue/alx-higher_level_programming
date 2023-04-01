a70769681044a775c28d1a15ef06de4c6461c446
Bekalu Endrias <bekalue2054@gmail.com>
2023-04-01 13:39:56 +0000

    🔧Fix variable mistake

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         response = requests.get(sys.argv[1])
-        if 'X-Request-Id' in requests.headers:
-            print(requests.headers['X-Request-Id'])
+        if 'X-Request-Id' in response.headers:
+            print(response.headers['X-Request-Id'])
