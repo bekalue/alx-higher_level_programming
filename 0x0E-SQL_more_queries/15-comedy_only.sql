1f3dc8511d39d6d8b1cbb659227cdab2338f9a69
Bekalu Geta <bekalu2054@gmail.com>
2023-05-30 21:13:36 +0300

    [fix]: 💁🏽`Make a right join`

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
 FROM tv_shows
-INNER JOIN tv_show_generes
+RIGHT JOIN tv_show_generes
 ON tv_shows.id = tv_show_generes.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_generes.genre_id
