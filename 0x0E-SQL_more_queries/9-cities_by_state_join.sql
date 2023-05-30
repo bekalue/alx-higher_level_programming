640076830109626d3f32098ae2649887bc641f8b
Bekalu Geta <bekalu2054@gmail.com>
2023-05-30 19:42:37 +0300

    feat]: upload city states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- a script that lists all cities contained in hbtn_0d_usa database
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
