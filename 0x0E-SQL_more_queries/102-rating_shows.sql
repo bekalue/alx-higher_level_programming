a080b51e3a39ddbc06b77b35e9517517cc75076d
Bekalu Geta <bekalu2054@gmail.com>
2023-05-30 22:55:57 +0300

    [feat]: fix spelling

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 RIGHT JOIN tv_show_ratings
-ON tv_show_rating.show_id = tv_shows.id
+ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
