42d1922162305ffd415fa0bf6f755d3506f3cebc
Bekalu Geta <bekalu2054@gmail.com>
2023-05-30 19:59:45 +0300

    [`fix`]: fix spelling error

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- select show title and genere id from hbtn_0d_tvshows database
 SELECT tv_shows.title, tv_show_genres.genere_id
 FROM tv_shows
-INNER JOIN tv_show_generes
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.tittle, tv_show_genres.genre_id ASC;
