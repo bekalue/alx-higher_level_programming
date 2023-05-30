1c1ad218e656fb7feee6b1de23f9a570d16de7e6
Bekalu Geta <bekalu2054@gmail.com>
2023-05-30 19:57:39 +0300

    [fix]: `fix spelling`

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- select show title and genere id from hbtn_0d_tvshows database
-SELECT tv_shows.title, tv_show_generes.genere_id
+SELECT tv_shows.title, tv_show_genres.genere_id
 FROM tv_shows
 INNER JOIN tv_show_generes
-ON tv_shows.id = tv_show_generes.show_id
-ORDER BY tv_shows.tittle, tv_show_generes.genre_id ASC;
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.tittle, tv_show_genres.genre_id ASC;
